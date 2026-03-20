c375076b7573d8f86866887153f92bb557005f31
pedrobuleje99 <pedrobuleje99@gmail.com>
2026-03-20 15:56:55 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Este SW se autodestruye y limpia todo
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
